6310cbbb4340ca35c100b618c55dbe5d45774db3
wilsonwolves <wilsonwolves@gmail.com>
2023-07-26 17:00:24 -0500

    as

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-import flask
-#from flask import Flask
+#import flask
+from flask import Flask
 import threading
 from flask import request
 import json
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/askdirector', methods=['post'])
 def askdirector_endpoint():
     payload = request.json   # payload should be {"input": "{text}"}    
-    return flask.Response(json.dumps({"status": "success"}), mimetype='application/json')
+    return json.dumps({"status": "success"})
 
 @app.route('/test', methods=['post'])
 def test_endpoint():
     payload = request.json   # payload should be {"input": "{text}"}    
     # return the payload as a response
-    return flask.Response(json.dumps(payload), mimetype='application/json')
+    return json.dumps(payload)
 
 @app.route('/test2', methods=['post'])
 def test_endpoint():
@@ -23,8 +23,6 @@ def test_endpoint():
     # return the payload as a response
     return payload
     
-
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Worldsy!'
