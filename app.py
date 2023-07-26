78e67b24c6d34281ed65db548f19020cbee92e77
wilsonwolves <wilsonwolves@gmail.com>
2023-07-26 16:47:00 -0500

    New endpoints

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+#from flask import Flask
+import threading
+from flask import request
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/askdirector', methods=['post'])
+def askdirector_endpoint():
+    payload = request.json   # payload should be {"input": "{text}"}    
+    return flask.Response(json.dumps({"status": "success"}), mimetype='application/json')
+
+@app.route('/test', methods=['post'])
+def test_endpoint():
+    payload = request.json   # payload should be {"input": "{text}"}    
+    # return the payload as a response
+    return flask.Response(json.dumps(payload), mimetype='application/json')
+
+@app.route('/test2', methods=['post'])
+def test_endpoint():
+    payload = request.json   # payload should be {"input": "{text}"}    
+    # return the payload as a response
+    return payload
+    
+
 
 @app.route('/')
 def hello_world():
