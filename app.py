fd7e751e83d776e1a2ad00b9e3cbe4ded0d8b151
wilsonwolves <wilsonwolves@gmail.com>
2023-07-26 17:34:48 -0500

    d

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #import flask
-from flask import Flask
+from flask import Flask, request, jsonify
 import threading
-from flask import request
 import json
 
 app = Flask(__name__)
@@ -13,3 +12,8 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route('/detect-language')
+def detect_language():
+    sentence = request.args.get('sentence')
+    return 'The detected language is: '+sentence
